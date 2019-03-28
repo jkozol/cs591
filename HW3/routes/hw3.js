b5f12d1bce922be9126a685c371b2c33759e5253
jkozol <jacobdkozol@gmail.com>
2019-03-28 15:00:41 -0400

    Update hw3

--- HW3/routes/hw3.js
+++ HW3/routes/hw3.js
@@ -5,9 +5,11 @@ const axios = require('axios');
 router.get('/hw3', function(req, res, next) {
   res.send(axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
     .then(response => {
-      const url = response.data.url;
-      const explanation = response.data.explanation;
-      console.log(url);
+      imageData = {
+        url: response.data.url,
+        explanation: response.data.explanation
+      }
+      res.send(imageData);
     })
     .catch(error => {
       console.log(error);
